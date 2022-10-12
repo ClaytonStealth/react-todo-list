30cc0a1bfe11f336006e594de842088e4931be84
claytonstealth <claytonses@yahoo.com>
2022-10-12 15:56:21 -0400

    first commit up to [4] in directions or [5]

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const ToDoListContainer = (props) => {
+  return (
+    <div>
+      <h1>
+        Todo List:{" "}
+        {props.toDoList.map((toDo, index) => {
+          return <ToDoItem toDo={toDo} key={index} />;
+        })}{" "}
+      </h1>
+    </div>
+  );
+};
+
+const ToDoItem = (props) => {
+  console.log(props.toDo);
+  return <div></div>;
+};
+
+const App = () => {
+  const [toDoList, settoDoList] = useState([
+    {
+      title: "Implement ToDo List",
+      priority: "High",
+      isComplete: false,
+      description: "Implement the todo list application",
+      creationDate: new Date().toString(),
+      completedDate: null,
+    },
+  ]);
+  return (
+    <div className='App-header'>
+      <ToDoListContainer toDoList={toDoList} />
+    </div>
+  );
+};
+
+export default App;
