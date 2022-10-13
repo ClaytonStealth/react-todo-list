9f3192792420a05f6f04d17631044312384fd746
claytonstealth <claytonses@yahoo.com>
2022-10-13 14:29:32 -0400

    some more work on the baseline for todo..working on  handleAddToDo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ToDoForm from "./component/ToDoForm";
 import "./App.css";
 
 const ToDoListContainer = (props) => {
@@ -18,16 +19,18 @@ const ToDoItem = (props) => {
   console.log(props.toDo);
   return (
     <div>
-      <h2>{props.title}</h2>
-      <p>{props.priority}</p>
-      <p>{props.creationDate}</p>
-      <p>{props.completedDate}</p>
+      <h2>{props.toDo.title}</h2>
+      <p>{props.toDo.priority}</p>
+      <p>{props.toDo.creationDate}</p>
+      {props.toDo.completedDate && (
+        <p>Completed Date: {props.toDo.completedDate}</p>
+      )}
     </div>
   );
 };
 
 const App = () => {
-  const [toDoList, settoDoList] = useState([
+  const [toDoList, setToDoList] = useState([
     {
       title: "Implement ToDo List",
       priority: "High",
@@ -37,8 +40,15 @@ const App = () => {
       completedDate: null,
     },
   ]);
+
+  const handleAddToDo = (toDo) => {
+    const toDoListCopy = [...toDoList];
+    toDoListCopy.push(toDo);
+    setToDoList(toDoListCopy);
+  };
   return (
     <div className='App-header'>
+      <ToDoForm />
       <ToDoListContainer toDoList={toDoList} />
     </div>
   );
---
+++ src/component/ToDoForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const ToDoForm = (props) => {
+  const [title, setTitle] = useState("");
+  const [priority, setPriority] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div>
+      <label>Title</label>
+      <input
+        type='text'
+        onChange={(e) => {
+          setTitle(e.target.value);
+          console.log(title);
+        }}
+      />
+      <br />
+      <label>Priority</label>
+      <select
+        onChange={(e) => {
+          setPriority(e.target.value);
+          console.log(priority);
+        }}
+      >
+        <option value=''></option>
+        <option value='Low'>Low</option>
+        <option value='Medium'>Medium</option>
+        <option value='High'>High</option>
+      </select>
+      <br />
+      <label>Description</label>
+      <textarea
+        onChange={(e) => {
+          setDescription(e.target.value);
+          console.log(description);
+        }}
+      ></textarea>
+      <br />
+    </div>
+  );
+};
+
+export default ToDoForm;
